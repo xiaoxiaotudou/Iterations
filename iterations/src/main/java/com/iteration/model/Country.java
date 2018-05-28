642b03fbc37e259c1c675d44cb94f0ca5907039f
Luke Chen <yt00335-49@localhost.localdomain>
2018-05-28 21:06:00 +0800

    [Iteration] Add City, Country, Customer, Film models and update BaseModel

--- iterations/src/main/java/com/iteration/model/Country.java
+++ iterations/src/main/java/com/iteration/model/Country.java
@@ -2,4 +2,22 @@ package com.iteration.model;
 
 public class Country extends BaseModel {
 
+    private int countryId;
+    private String country;
+
+    public int getCountryId() {
+        return countryId;
+    }
+
+    public void setCountryId(int countryId) {
+        this.countryId = countryId;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
 }
